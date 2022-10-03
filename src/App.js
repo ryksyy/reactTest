5d93dc28b4135fe814cf8a42532dc493eea77879
ryksyy <miko.prykari@gmail.com>
2022-10-03 21:03:05 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [hrll, setHRLLow] = useState(0);
+  const [hrlh, setHRLHigh] = useState(0);
+  function calculate()  {
+    setHRLLow((220-age)*0.65);
+    setHRLHigh((220-age)*0.85);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HEART LIMITS CALCULATOR</h1>
+      <form>
+        <div>
+          <label>Age</label><br></br>
+          <input type="number" value={age} onChange={e=> setAge(e.target.value)}/><br></br><br></br>
+        </div>
+        <div>
+          <label>Heart rate limits</label><br></br>
+          <output>{hrll + "-" + hrlh}</output>
+        </div>
+        <button type="button" onClick={calculate}>Calculate</button>
+      </form>
     </div>
-  );
+  )
 }
+
 
 export default App;
